a228b0bf8520423070cdc7cafbb90ddc7369210a
kapyanammu1 <xtianized1@gmail.com>
2023-07-08 03:17:56 +0800

    df

--- SIS/settings.py
+++ SIS/settings.py
@@ -13,8 +13,13 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env()
+
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -23,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pm!z)c+%ewzctnknrpd32xz**ne=4^)wb%np^@vvolvlgw&god'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -92,7 +97,7 @@ WSGI_APPLICATION = 'SIS.wsgi.application'
 
 
 DATABASES = {
-    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': dj_database_url.parse(env('DATABASE_URL'))
 }
 
 
