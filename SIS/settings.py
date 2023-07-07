6fa48db5525c1d9666a59b83fbc4a88813b5893c
kapyanammu1 <xtianized1@gmail.com>
2023-07-08 02:48:01 +0800

    sd

--- SIS/settings.py
+++ SIS/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'SIS.wsgi.application'
 #     }
 # }
 
-import dj_database_url
+
 DATABASES = {
     "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
